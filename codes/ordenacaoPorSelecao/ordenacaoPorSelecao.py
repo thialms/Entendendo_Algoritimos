558a6335c7770dd8b092ea7237b2f06dd0372896
Thiago de Almeida Souza Silva <thiiago.allmeida44@gmail.com>
2025-08-19 05:19:23 -0300

    Create ordenacaoPorSelecao.py

---
+++ codes/ordenacaoPorSelecao/ordenacaoPorSelecao.py
@@ -0,0 +1,17 @@
+def buscaMenor(arr):
+    menor = arr[0]
+    menor_indice = 0
+    for i in range(1, len(arr)):
+        if arr[i] < menor:
+            menor = arr[i]
+            menor_indice = i
+    return menor_indice
+    
+def ordenacaoPorSelecao(arr):
+    newArr = []
+    for i in range(len(arr)):
+        menor = buscaMenor(arr)
+        newArr.append(arr.pop(menor))
+    return newArr
+    
+print(ordenacaoPorSelecao([5, 3, 6, 2, 10]))
